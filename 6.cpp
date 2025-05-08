0266329edaeec770133e0e99ac0f1791c2fc7273
josss8 <122937940+josss8@users.noreply.github.com>
2025-05-08 09:29:48 -0600

    Update 6.cpp

--- 6.cpp
+++ 6.cpp
@@ -4,29 +4,29 @@ using namespace std;
 int main() {
 	int decision;
 	
-	cout << "Eres un cirujano con un órgano vital disponible para trasplante.\n";
-	cout << "Solo puedes salvar a una persona con él, pero hay varios pacientes esperándo.\n";
-	cout << "¿A quién salvas?\n";
-	cout << "1. Un científico brillante que podría curar enfermedades.\n";
-	cout << "2. Un niño de 5 años.\n";
-	cout << "3. Una madre soltera con tres hijos pequeños.\n";
+	cout << "Eres un cirujano con un Ã³rgano vital disponible para trasplante.\n";
+	cout << "Solo puedes salvar a una persona con Ã©l, pero hay varios pacientes esperÃ¡ndo.\n";
+	cout << "Â¿A quiÃ©n salvas?\n";
+	cout << "1. Un cientÃ­fico brillante que podrÃ­a curar enfermedades.\n";
+	cout << "2. Un niÃ±o de 5 aÃ±os.\n";
+	cout << "3. Una madre soltera con tres hijos pequeÃ±os.\n";
 	cout << "4. Un anciano .\n";
 	cout << "5. dejas que el sistema de salud elija a alguien random.\n";
 	
 	cin >> decision;
 	
 	if (decision == 1) {
-		cout << "Salvaste al científico. Podría revolucionar la medicina, pero los demás murieron.\n";
+		cout << "Salvaste al cientÃ­fico. PodrÃ­a revolucionar la medicina, pero los demÃ¡s murieron.\n";
 	} else if (decision == 2) {
-		cout << "Salvaste al niño. Tiene toda la vida por delante.\n";
+		cout << "Salvaste al niÃ±o. Tiene toda la vida por delante.\n";
 	} else if (decision == 3) {
-		cout << "Salvaste a la madre. Sus hijos no quedaron huérfanos.\n";
+		cout << "Salvaste a la madre. Sus hijos no quedaron huÃ©rfanos.\n";
 	} else if (decision == 4) {
-		cout << "Salvaste al anciano, pero su tiempo era limitado en comparación con otros.\n";
+		cout << "Salvaste al anciano, pero su tiempo era limitado en comparaciÃ³n con otros.\n";
 	} else if (decision == 5) {
-		cout << "El sistema eligió, Tal vez fue justo, pero otros no lo verán así.\n";
+		cout << "El sistema eligiÃ³, Tal vez fue justo, pero otros no lo verÃ¡n asÃ­.\n";
 	} else {
-		cout << "Decisión inválida.\n";
+		cout << "DecisiÃ³n invÃ¡lida.\n";
 	}
 	
 	
